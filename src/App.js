3282410a9885f2e251b9fe95af83ea4f55f5683b
Vasanth Kumar <vasanthvc02@gmail.com>
2024-12-11 20:46:22 +0530

    Normalize line endings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/appbar/Appbar'
 import Footer from './components/footer/Footer';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div>
       <Header />
-
+      <Home />
       <Footer />
     </div>
   );
